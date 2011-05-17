27cba4b5e2c158afaa66728b3513b76cb9563600
Tim Bielawa <tbielawa@redhat.com>
2011-05-17 13:42:55 -0400

    Cleanup. Template of next command.

--- src/nagios.py
+++ src/nagios.py
@@ -38,7 +38,7 @@ class Nagios(func_module.FuncModule):
         """
         Schedule downtime for the target services on host.
         """
-        
+
         dt_start = int(time.time())
         dt_duration = (minutes * 60)
         dt_end = dt_start + dt_duration
@@ -48,7 +48,7 @@ class Nagios(func_module.FuncModule):
         dt_trigger = 0
 
         nagios_return = True
-        
+
         for service in targets:
             # This kinda sucks but... what can you do...
             #
@@ -65,6 +65,45 @@ class Nagios(func_module.FuncModule):
             return "OK"
         else:
             return "Fail: could not write to command file"
+
+    def schedule_host_downtime(self, host, minutes=30):
+        """
+        Schedule downtime for the target host.
+        """
+
+        dt_start = int(time.time())
+        dt_duration = (minutes * 60)
+        dt_end = dt_start + dt_duration
+        dt_user = "func"
+        dt_comment = "Scheduling downtime"
+        dt_fixed = 1
+        dt_trigger = 0
+
+        nagios_return = True
+
+        for service in targets:
+            # This kinda sucks but... what can you do...
+            #
+            # [start_time] SCHEDULE_SVC_DOWNTIME;<host_name>;
+            # <service_desription>;<start_time>;<end_time>;<fixed>;
+            # <trigger_id>;<duration>;<author>;<comment>
+            dt_args = ["SCHEDULE_SVC_DOWNTIME", host, service, str(dt_start),
+                       str(dt_end), str(dt_fixed), str(dt_trigger), str(dt_duration), dt_user,
+                       dt_comment]
+            dt_command = "[" + str(dt_start) + "] " + ";".join(dt_args) + "\n"
+            nagios_return = nagios_return and self._write_command(dt_command)
+
+        if nagios_return:
+            return "OK"
+        else:
+            return "Fail: could not write to command file"
+
+    def schedule_host_downtime(self, host):
+        """
+        Schedule downtime for a given host
+        """
+        # SCHEDULE_HOST_DOWNTIME;<host_name>;<start_time>;<end_time>;<fixed>;<trigger_id>;<duration>;<author>;<comment>
+        pass
 
     def enable_alerts(self, host):
         """
